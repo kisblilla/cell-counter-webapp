e76c7e4c9c21227f02085b832f1fe18a1bd8627a
kisb.lilla <kisb.lilla@gmail.com>
2023-12-09 01:23:35 +0100

    refactor: newly trained model added

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,8 @@ WORKDIR /app
 ADD . /app
 
 # Install dependencies
-RUN pip install -r requirements.txt
 RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+RUN pip install -r requirements.txt
 
 # copying all files over
 COPY . /app
--- app.py
+++ app.py
@@ -7,11 +7,12 @@ import cv2
 import tensorflow as tf
 from PIL import Image
 import time
+import os
 
 def run():
     
-    st.title("Vérsejt számláló")
-    st.text("Megadja, hogy adott képen hány darab vérsejt látható")
+    st.title("Cell counter")
+    st.text("Detect and count the cells on images")
         
     option = st.selectbox(
     'Kép feltöltése vagy alapértelmezett képek közül választás:',
@@ -19,7 +20,9 @@ def run():
 
     IMG_HEIGHT = 128
     IMG_WIDTH = 128
+    test_id_list = os.listdir('./img')
     
+
     if option == 'Feltöltés':
         data_file = st.file_uploader("Kép feltöltése")
 
@@ -27,22 +30,19 @@ def run():
             image_array = Image.open(data_file)
             st.image(image_array)
         else:
-            image_array = Image.open("SIMCEPImages_A01_C1_F1_s01_w1.TIF")
+            image_array = Image.open(f'./img/{test_id_list[0]}')
         with st.spinner('Kép feldolgozása...'):
             time.sleep(3)
         st.success('Done!')
 
         
     else:
-
-        test_id_list = ["SIMCEPImages_A01_C1_F1_s01_w1.TIF", "SIMCEPImages_A11_C44_F1_s21_w1.TIF", "SIMCEPImages_A19_C78_F1_s25_w2.TIF"]
-
         cv2_image_jpg_list = []
         for i in range(len(test_id_list)):
-            img = cv2.imread(test_id_list[i])
+            img = cv2.imread(f'./img/{test_id_list[i]}')
             img_resized = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH))
-            cv2.imwrite(f'image_{i}.jpg', img_resized)
-            cv2_image_jpg_list.append(f'image_{i}.jpg')
+            cv2.imwrite(f'./tmp/image_{i}.jpg', img_resized)
+            cv2_image_jpg_list.append(f'./tmp/image_{i}.jpg')
         
 
         img = image_select("Label", cv2_image_jpg_list)
@@ -55,17 +55,17 @@ def run():
             time.sleep(3)
         st.success('Done!')
 
-        image_array = Image.open(test_id_list[index_of_selected_image])
-        
+        image_array = Image.open(f'./img/{test_id_list[index_of_selected_image]}')
+ 
     if image_array is not None:
             
-        img = image_array.save("img.jpg")
-        img = cv2.imread("img.jpg")
+        img = image_array.save("./tmp/img_tmp.jpg")
+        img = cv2.imread("./tmp/img_tmp.jpg")
         
     else: 
-        image_array = Image.open(test_id_list[0])
+        image_array = Image.open(f'./img/{test_id_list[0]}')
 
-    model = tf.keras.models.load_model('CellCounterModel.h5')
+    model = tf.keras.models.load_model('./train_results/model.h5')
     
     IMG_CHANNELS = 3
     X_test = np.zeros((1, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)) #, dtype=np.bool)
@@ -73,27 +73,27 @@ def run():
     if option == 'Feltöltés':
 
         cv2_image = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH))
-        cv2.imwrite('mask.jpg', cv2_image)
-        mask = cv2.imread('mask.jpg')
+        cv2.imwrite('./tmp/img_tmp.jpg', cv2_image)
+        img_tmp = cv2.imread('./tmp/img_tmp.jpg')
         
-        X_test[0] = mask
+        X_test[0] = img_tmp
         
         preds = model.predict(X_test)
         
         result = ' Pred: ' + str(int(preds))
         plt.title(result, fontsize=14)
         plt.axis('off')
-        plt.savefig(f'tmp_test.png')
-        st.image('tmp_test.png')
+        plt.savefig(f'./tmp/tmp_test.png')
+        st.image('./tmp/tmp_test.png')
         
     else:
 
-        path_mask = './BBBC005_v1_ground_truth/' + test_id_list[index_of_selected_image]
+        path_img = './img/' + test_id_list[index_of_selected_image]
 
-        cv2_image = cv2.imread(path_mask)
+        cv2_image = cv2.imread(path_img)
         cv2_image = cv2.resize(cv2_image, (IMG_HEIGHT, IMG_WIDTH))
-        cv2.imwrite('mask.jpg', cv2_image)
-        mask = cv2.imread('mask.jpg')
+        cv2.imwrite('./tmp/img_tmp.jpg', cv2_image)
+        mask = cv2.imread('./tmp/img_tmp.jpg')
     
         X_test[0] = mask
 
@@ -103,9 +103,9 @@ def run():
         result = ' Pred: ' + str(int(preds))
         plt.title(result, fontsize=14)
         plt.axis('off')
-        plt.savefig(f'tmp_test.png')
+        plt.savefig(f'./tmp/tmp_test.png')
 
-        st.image('tmp_test.png')
+        st.image('./tmp/tmp_test.png')
         
         
 
